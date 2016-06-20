658448d863e3bc59d0a796053fd46f7987c504f2
Steven <steven_kissack@pokerstars.com>
2016-06-20 13:34:52 +0100

    update gulpfile to reflect new structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
+'use strict';
+
 const gulp = require('gulp')
 const sourcemaps = require('gulp-sourcemaps')
 const babel = require('gulp-babel')
 const concat = require('gulp-concat')
-const browserify = require('browserify')
+const rename = require('gulp-rename')
+const htmlify = require('gulp-angular-htmlify')
+const uglify = require('gulp-uglify')
+const html2js = require("gulp-html2js")
+const gutil = require('gulp-util')
+const merge = require('merge-stream')
 const source = require('vinyl-source-stream') // Used to bypass need for gulp-browserify
 const del = require('del')
 
@@ -12,6 +19,9 @@ const jsGlob = 'src/**/*.js'
 const htmlGlob = 'src/**/*.html'
 
 const demoGlob = ['app/demo/**/*.html', 'app/demo/**/*.js', 'app/demo/**/*.css']
+
+// the newline is needed in case the file ends with a line comment, the semi-colon is needed if the last statement wasn't terminated
+const jsConcatOptions = { newLine : '\n;' }
 
 gulp.task('browserSync', () => {
   browserSync.init({
@@ -35,10 +45,27 @@ gulp.task('transpile', ['cleanTranspileFolder'], () => {
 })
 
 gulp.task('compile', ['transpile'], () => {
-	return browserify(['dist/transpiled/stamp.js'])
-	.bundle()
-    .pipe(source('stamp-bundle.js'))
-    .pipe(gulp.dest('dist'))
+
+  let jsFileStream = gulp.src('./dist/transpiled/**/*.js')
+    //.pipe(gulpif(doMinification, ngmin().on('error', gutil.log))) // Corrects shorthand DI to array syntax
+    //.pipe(concat('app.js'))
+    //.pipe(gulp.dest('./dist/app/js'))
+    
+  let tmplFileStream = gulp.src('./src/angular/templates/*.html')
+		.pipe(htmlify()) // Add data- prefixes to non-html5-valid attributes
+		.pipe(html2js({
+			  outputModuleName: "stamp"
+			}))
+
+  let appStream = merge(tmplFileStream, jsFileStream)
+    .pipe(concat('bundle.js', jsConcatOptions))
+
+    .pipe(gulp.dest("dist/angular/"))
+    .pipe(uglify().on('error', gutil.log))
+    .pipe(rename('bundle.min.js'))
+    .pipe(gulp.dest("dist/angular/"))
+  
+  return appStream
 })
 
 gulp.task('watch', ['browserSync', 'compile'], () => {
